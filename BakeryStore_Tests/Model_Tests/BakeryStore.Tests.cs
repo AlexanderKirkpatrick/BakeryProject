bd4a6db1f0ec66982b836ae6d10dca64a6836206
Alexander <knibz@protonmail.com>
2022-07-02 12:10:53 -0400

    add first test with empty method

--- BakeryStore_Tests/Model_Tests/BakeryStore.Tests.cs
+++ BakeryStore_Tests/Model_Tests/BakeryStore.Tests.cs
@@ -1,2 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BakeryStore.Models;
+
+namespace BakeryStore.TestTools
+{
+  [TestClass]
+  public class BreadsTests
+  {
+    [TestMethod]
+    public void AmountOfBread_CreatesInstanceOfBread_Breads()
+    {
+      
+    }
+  }
+}
