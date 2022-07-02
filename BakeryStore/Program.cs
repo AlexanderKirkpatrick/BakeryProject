71a0fc35c2245e9c1ae76723dcf8d70df68a8279
Alexander <knibz@protonmail.com>
2022-07-02 16:05:26 -0400

    add namespace, class, Main, and welcome writeline

--- BakeryStore/Program.cs
+++ BakeryStore/Program.cs
@@ -1,2 +1,14 @@
 using System;
 using BakeryStore.Models;
+
+namespace StoreFront
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!!");
+    }
+
+  }
+}
