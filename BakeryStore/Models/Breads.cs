6d53c61e4db11117b6cd8efee4eb8b97d10dbf5d
Alexander <knibz@protonmail.com>
2022-07-02 14:16:37 -0400

    add BreadCount int, with Bread and Cost methods, to public class Bread

--- BakeryStore/Models/Breads.cs
+++ BakeryStore/Models/Breads.cs
@@ -1,1 +1,31 @@
 using System;
+
+namespace BakeryStore.Models
+{
+    public class Bread
+  {
+
+    public int BreadCount {get; set; }
+    public Bread (int breadCount)
+    {
+      BreadCount = breadCount;
+    }
+
+    public int Cost ()
+    {
+      int cost = 0;
+      int loaves = BreadCount;
+
+      if (loaves >= 3)
+      {
+        int loafTriples = loaves / 3;
+        cost += 10 * loafTriples;
+        loaves -= (loafTriples * 3);
+      }
+
+      cost += (5 * loaves);
+
+      return cost;
+    }
+  }
+}
