7ca29111935e23f1fea935ccad73817864b6c7b6
Alexander <knibz@protonmail.com>
2022-07-02 18:02:56 -0400

    add order methods, edit spacing and alignment

--- BakeryStore/Program.cs
+++ BakeryStore/Program.cs
@@ -26,7 +26,65 @@ namespace BakeryStore
       Console.WriteLine("");
       Console.WriteLine("               Buy two loaves of bread, get one free!!");     
       Console.WriteLine("       Buy three pastries, receive third pastry at half price!!"); 
+
+      int breadCost = BreadOrder();
+      int pastryCost = PastryOrder();
+
+      if (breadCost == 0 && pastryCost == 0)
+      {
+        Console.WriteLine("           Thanks for checking us out! Please come again!!");
+      } 
+      else
+      {
+        Console.WriteLine($"                      Your order total is ${breadCost + pastryCost}");
+      }
     }
 
+    private static int BreadOrder()
+    {
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("           How many loaves of bread would you like today?");
+      int loafCount = GetOrder();
+      Bread breadOrder = new Bread(loafCount);
+      int breadCost = breadOrder.Cost();
+      return breadCost;
+    }
+
+    private static int PastryOrder()
+    {
+      Console.WriteLine("              How many pastries would you like today?");
+      int pastryCount = GetOrder();
+      Pastries pastryOrder = new Pastries(pastryCount);
+      int pastryCost = pastryOrder.Cost();
+      return pastryCost;
+    }
+
+    private static int GetOrder()
+    {
+      string userInput = Console.ReadLine();
+      int order;
+      bool canBeParsed = int.TryParse(userInput, out order);
+
+      if (canBeParsed)
+      {
+        if(order < 0)
+        {
+          Console.WriteLine("               Please submit a number 0 or greater:");
+          order = GetOrder();
+          return order;
+        }
+        else 
+        {
+          return order;
+        }
+      }
+      else
+      {
+        Console.WriteLine("      Non-number characters are not permitted. Please resubmit:");
+        order = GetOrder();
+        return order;
+      }
+    }
   }
 }
