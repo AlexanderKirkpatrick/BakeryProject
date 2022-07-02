8fac719f22dbf9b641503a66bda9f57280be9b05
Alexander <knibz@protonmail.com>
2022-07-02 14:59:49 -0400

    add passing PastryTest methods

--- BakeryStore_Tests/Model_Tests/BakeryStore.Tests.cs
+++ BakeryStore_Tests/Model_Tests/BakeryStore.Tests.cs
@@ -34,4 +34,36 @@ namespace BakeryStore.TestTools
       Assert.AreEqual(20, testBread.Cost());
     }
   }
+
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastriesConstructor_CreatesInstanceOfPastries_PastriesObject()
+    {
+      Pastries testPastry = new Pastries(3);
+      Assert.AreEqual(typeof(Pastries), testPastry.GetType());
+    }
+
+    [TestMethod]
+    public void PastriesConstructor_CreatesInstanceOfPastriesWithPastryCount_Int()
+    {
+      Pastries testPastry = new Pastries(3);
+      Assert.AreEqual(3, testPastry.PastryCount);
+    }
+
+    [TestMethod]
+    public void Cost_ReturnsCostOfOrder_Int()
+    {
+      Pastries testPastry = new Pastries(7);
+      Assert.AreEqual(12, testPastry.Cost());
+    }
+
+    [TestMethod]
+    public void Cost_ReturnsCostOfOrderWithDiscount_Int()
+    {
+      Pastries testPastry = new Pastries(3);
+      Assert.AreEqual(5, testPastry.Cost());
+    }
+  }
 }
