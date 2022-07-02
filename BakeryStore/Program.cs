0b916f0a17ce341843df92f7a2bf394c369402bc
Alexander <knibz@protonmail.com>
2022-07-02 16:46:12 -0400

    add fancy welcome intro and menu with specials

--- BakeryStore/Program.cs
+++ BakeryStore/Program.cs
@@ -1,13 +1,31 @@
 using System;
 using BakeryStore.Models;
 
-namespace StoreFront
+namespace BakeryStore
 {
   public class Program
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!!");
+      Console.WriteLine("                  Welcome to Pierre's Bakery!!");
+      Console.WriteLine("");
+      Console.WriteLine(@"                             \\\\            
+                           _( *v* )->        
+	                 _(         )_     
+                        (             )  
+                       mmmmmmmmmmmmmmmmm ");
+      Console.WriteLine("");
+      Console.WriteLine("                              MENU");
+      Console.WriteLine("                            ________");
+      Console.WriteLine("");    
+      Console.WriteLine("                     Bread:  $5 (single loaf) ");  
+      Console.WriteLine("                    Pastry:  $2 (single pastry) ");   
+      Console.WriteLine("");
+      Console.WriteLine("                            SPECIALS"); 
+      Console.WriteLine("                           __________"); 
+      Console.WriteLine("");
+      Console.WriteLine("               Buy two loaves of bread, get one free!!");     
+      Console.WriteLine("       Buy three pastries, receive third pastry at half price!!"); 
     }
 
   }
