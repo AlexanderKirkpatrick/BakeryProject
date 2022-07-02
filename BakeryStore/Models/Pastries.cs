6aba67f170c0d7f48232f48a1bc4a27bf254b6ba
Alexander <knibz@protonmail.com>
2022-07-02 15:08:18 -0400

    add Pastries class, with Pastries and cost methods

--- BakeryStore/Models/Pastries.cs
+++ BakeryStore/Models/Pastries.cs
@@ -1,1 +1,30 @@
 using System;
+
+namespace BakeryStore.Models
+{
+  public class Pastries
+  {
+    public int PastryCount {get; set; }
+    public Pastries (int pastryCount)
+    {
+      PastryCount = pastryCount;
+    }
+
+    public int Cost ()
+    {
+      int cost = 0;
+      int pastryCount = PastryCount;
+
+      if (pastryCount >= 3)
+      {
+        int pastryTriples = pastryCount / 3;
+        cost += 5 * pastryTriples;
+        pastryCount -= (pastryTriples * 3);
+      }
+
+      cost += (2 * pastryCount);
+
+      return cost;
+    }
+  }
+}
